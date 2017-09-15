7c8ba26642a994bd4b27b406c63fb7f5d34fdbb9
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-09-16 03:07:18 +0800

    Classes implement Printable interface

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -9,12 +9,13 @@ import java.util.List;
  * Represents a Person's name in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
  */
-public class Name {
+public class Name implements Printable {
 
     public static final String EXAMPLE = "John Doe";
     public static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphanumeric characters";
     public static final String NAME_VALIDATION_REGEX = "[\\p{Alnum} ]+";
-
+    public static final String NAME_PRINTABLE = "Names: ";
+    
     public final String fullName;
 
     /**
@@ -60,5 +61,9 @@ public class Name {
     public int hashCode() {
         return fullName.hashCode();
     }
-
+    
+    @Override
+    public String getPrintableString() {
+        return NAME_PRINTABLE + fullName;
+    }
 }
