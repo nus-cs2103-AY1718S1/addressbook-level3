d307b54d11b826ef01b8e7ae0beb7b0411f286cc
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-09-16 15:42:19 +0800

    Add abstract isMutating to Command superclass and implement it in all subclasses

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -47,6 +47,14 @@ public abstract class Command {
     //public abstract CommandResult execute();
 
     /**
+     * Checks whether this command mutates the data so as to decide whether to save the changes to the storage file
+     * after this command is executed.
+     *
+     * @return true if the command mutates the data.
+     */
+    public abstract boolean isMutating();
+
+    /**
      * Supplies the data the command will operate on.
      */
     public void setData(AddressBook addressBook, List<? extends ReadOnlyPerson> relevantPersons) {
