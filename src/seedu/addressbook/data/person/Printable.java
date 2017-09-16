fddffe6e1e05921e7e85a569270ab773b2ca5025
Kyle Chu <ktnc.985@gmail.com>
2017-09-16 14:34:09 +0800

    Create Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+/**
+ * A Printable interface for each type of contact information in the address book
+ */
+public interface Printable {
+
+    String getPrintableString();
+}
