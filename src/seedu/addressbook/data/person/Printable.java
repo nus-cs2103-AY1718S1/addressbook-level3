94ca866044362b157c69da3bfed236b5a221237a
Tan Zhen Yong <Xenonym@users.noreply.github.com>
2017-09-16 16:52:58 +0800

    Add Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface to provide printable strings for person data classes.
+ */
+public interface Printable {
+    /**
+     * Returns a printable string representation of the object.
+     */
+    String getPrintableString();
+}
