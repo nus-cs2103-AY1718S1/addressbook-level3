a51543e414032a991640cd84707f7df6de1e162a
itsdickson <itsklein@hotmail.com>
2017-09-17 03:33:20 +0800

    Command.java added method isMutating

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -43,6 +43,10 @@ public abstract class Command {
         throw new UnsupportedOperationException("This method should be implement in child classes");
     }
 
+    public boolean isMutating() {
+        return true;
+    }
+
     //Note: it is better to make the execute() method abstract, by replacing the above method with the line below:
     //public abstract CommandResult execute();
 
