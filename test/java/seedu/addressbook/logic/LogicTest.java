1bf64655a72099825bef847dad6dc4cf09e26893
Henry Ang <FrozVentus@users.noreply.github.com>
2017-09-17 14:52:08 +0800

    [W6.1d] 4. Updated LogicTest to account for changes in the saving method Also added missing newline to HelpCommand class for consistency

--- test/java/seedu/addressbook/logic/LogicTest.java
+++ test/java/seedu/addressbook/logic/LogicTest.java
@@ -12,6 +12,7 @@ import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.*;
 import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.parser.Parser;
 import seedu.addressbook.storage.StorageFile;
 
 import java.util.*;
@@ -79,6 +80,11 @@ public class LogicTest {
 
         //Execute the command
         CommandResult r = logic.execute(inputCommand);
+        Command command = new Parser().parseCommand(inputCommand);
+        if (!command.isMutating()) {
+            saveFile.save(addressBook);
+        }
+
 
         //Confirm the result contains the right data
         assertEquals(expectedMessage, r.feedbackToUser);
