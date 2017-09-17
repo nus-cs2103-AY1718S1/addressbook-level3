0a4e8d60f6024317561707f8ac886cc1709b1e00
ALPHEUS\Alpheus Lim <e0031928@u.nus.edu>
2017-09-17 13:41:43 +0800

    added appication icon

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -2,6 +2,7 @@ package seedu.addressbook.ui;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.Main;
@@ -32,6 +33,7 @@ public class Gui {
     public void start(Stage stage, Stoppable mainApp) throws IOException {
         mainWindow = createMainWindow(stage, mainApp);
         mainWindow.displayWelcomeMessage(version, logic.getStorageFilePath());
+
     }
 
     private MainWindow createMainWindow(Stage stage, Stoppable mainApp) throws IOException{
@@ -43,6 +45,8 @@ public class Gui {
         loader.setLocation(Main.class.getResource("ui/mainwindow.fxml"));
 
         stage.setTitle(version);
+        // Set the application icon.
+        stage.getIcons().add(new Image("file:resources/images/if_Address_Book_86957.png"));
         stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
         stage.show();
         MainWindow mainWindow = loader.getController();
