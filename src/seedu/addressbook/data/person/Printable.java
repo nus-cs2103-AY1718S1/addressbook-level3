756fc65d200bb9e7c50cec3ed2f91c329e56e015
Jo Brough <fr3d3rickfr3d3rickson@gmail.com>
2017-09-18 21:39:34 +0800

    Added the static method Printable.getPrintableString that can convert many Printables into a single string

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -3,4 +3,17 @@ package seedu.addressbook.data.person;
 public interface Printable {
 
     String getPrintableString();
+
+    /**
+     * converts multiple Printables into a single string, separated by spaces
+     * @param printables is a varargs collection of Printables
+     * @return a string representing the arguments, in order, separated by spaces
+     */
+    static String getPrintableString(Printable... printables) {
+        String output = "";
+        for (Printable p : printables) {
+            output += p.getPrintableString() + " ";
+        }
+        return output;
+    }
 }
