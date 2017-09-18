1b014945279af602ff51389edac50e565cb3d1e1
shunyong <e0002695@u.nus.edu>
2017-09-18 19:20:07 +0800

    update output format

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -9,7 +9,7 @@ public class SortCommand extends Command{
 
     public static final String COMMAND_WORD = "sort";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
             + ": Displays all persons in the address book as a list with index numbers in sorted order.\n"
             + "Example: " + COMMAND_WORD;
 
