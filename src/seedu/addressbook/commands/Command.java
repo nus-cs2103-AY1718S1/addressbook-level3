3406151a6b72c7023d3df6d228301175d9a0b237
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2017-09-19 02:56:18 +0800

    Add isMutating() method and override it in Add, Clear and Delete command

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,10 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    public boolean isMutating() {
+        return false;
+    }
 
     /**
      * Supplies the data the command will operate on.
