a2384b0f9fe7a8fcade3b781c644d0a1555601f8
LAPTOP-DK3JDSV0\wishingmaid <pigironsteel@gmail.com>
2017-09-18 22:30:14 +0800

    added isMutating() to child classes that extend Command class

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -65,4 +65,8 @@ public abstract class Command {
     public void setTargetIndex(int targetIndex) {
         this.targetIndex = targetIndex;
     }
+    
+    public boolean isMutating(){
+        throw new UnsupportedOperationException("to be implemented in child class");
+    }
 }
