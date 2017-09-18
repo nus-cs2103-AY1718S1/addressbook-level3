f831837676b13f0d09f8da3ac52443265a92f66b
quangtdn <nmquang229@yahoo.com.vn>
2017-09-19 03:48:08 +0800

    add interface Printable

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+import seedu.addressbook.data.tag.Tag;
+import seedu.addressbook.data.tag.UniqueTagList;
+/**
+ * Created by nguyenminhquang on 9/19/17.
+ */
+public interface Printable {
+    String getPrintableString();
+}
