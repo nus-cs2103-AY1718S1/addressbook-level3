95fd71ba4e0eb220837f94ec58bb9980ac12f16f
Pj Poh <e0037036@u.nus.edu>
2017-09-18 19:42:15 +0800

    Remove unnecessary import statements.

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,8 +1,5 @@
 package seedu.addressbook.data.person;
 
-import seedu.addressbook.data.tag.Tag;
-import seedu.addressbook.data.tag.UniqueTagList;
-
 public interface Printable {
     String getPrintableString();
 }
