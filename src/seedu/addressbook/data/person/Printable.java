bf61096bcddafbe0bf67cf9bf6050412b7f6b6f1
Alex Ang <E0032221@u.nus.edu>
2017-09-18 13:55:25 +0800

    Update Printable Interface to specifications
    For LO-Interfaces

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,4 +1,8 @@
 package seedu.addressbook.data.person;
 
 public interface Printable {
+    /**
+     * Returns a printable string representation of the data type of a person
+     */
+    String getPrintableString();
 }
