7a22f4c55ef4fe14e3e90244bc047f1318f3139a
Pj Poh <e0037036@u.nus.edu>
2017-09-18 19:31:32 +0800

    Add Printable interface.

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.tag.Tag;
+import seedu.addressbook.data.tag.UniqueTagList;
+
+public interface Printable {
+    String getPrintableString();
+}
