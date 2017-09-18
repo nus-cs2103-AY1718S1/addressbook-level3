a3c12fe44282ff2189fbe9382465b98d8b174a97
shunyong <e0002695@u.nus.edu>
2017-09-18 19:17:23 +0800

    1.Add method to call the sort method in Addressbook.java 2. Update execute method to return success message

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,4 +1,25 @@
 package seedu.addressbook.commands;
 
-public class SortCommand {
+
+/**
+ * Sort the address book based on the name of the contact.
+ */
+
+public class SortCommand extends Command{
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book as a list with index numbers in sorted order.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "List sorted successfully";
+
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
 }
