7f7ed0bb592480ea37b46e598cd216c9d25b9226
Ruchir Aggrawal <e0216119@u.nus.edu>
2017-09-18 17:49:21 +0800

    Printable.java: add getPrintableString method
    add Name class implement Printable interface
    Name.java: add getPrintableString method to override

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,4 +1,11 @@
 package seedu.addressbook.data.person;
 
+/**
+ * An interface used by Name, Phone, Email and address
+ */
 public interface Printable {
+    
+    public String getPrintableString();
+    
 }
+
