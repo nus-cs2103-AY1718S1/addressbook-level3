c2510db1bb92d0460ad46ea759379a7d7d55a726
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-09-19 11:11:55 +0800

    change the scope of the 'getPrintableString' method to package

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -2,7 +2,7 @@ package seedu.addressbook.data.person;
 
 public interface Printable {
 
-    public default String getPrintableString() {
+    default String getPrintableString() {
         return null;
     }
 
