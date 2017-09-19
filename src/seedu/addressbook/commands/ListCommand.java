04ea4a21851fd8c774c1b4fa161ab331b20eee27
YanWC <e0015002@u.nus.edu>
2017-09-19 17:49:31 +0800

    Removed unnecessary changes

--- src/seedu/addressbook/commands/ListCommand.java
+++ src/seedu/addressbook/commands/ListCommand.java
@@ -1,11 +1,7 @@
 package seedu.addressbook.commands;
 
-import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 
@@ -16,28 +12,14 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
             + "Displays all persons in the address book as a list with index numbers.\n\t"
             + "Example: " + COMMAND_WORD;
 
 
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = getAddressList(addressBook.getAllPersons().iterator());
-        sortListByName(allPersons);
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
-    }
-
-    private void sortListByName(List<ReadOnlyPerson> allPersons) {
-        Collections.sort(allPersons, (ReadOnlyPerson person1, ReadOnlyPerson person2) ->
-                (person1.getName().toString()).compareTo(person2.getName().toString()));
-    }
-
-    private List<ReadOnlyPerson> getAddressList(Iterator<Person> iterator) {
-        List<ReadOnlyPerson> list = new ArrayList<>();
-        while (iterator.hasNext()) {
-            list.add(iterator.next());
-        }
-        return list;
     }
 }
