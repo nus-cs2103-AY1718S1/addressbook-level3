aee5ef5831310ad8e564ffbe9d3f0193719c5468
ALPHEUS\Alpheus Lim <e0031928@u.nus.edu>
2017-09-19 23:20:59 +0800

    added list, clear and exit button

--- src/seedu/addressbook/ui/MainWindow.java
+++ src/seedu/addressbook/ui/MainWindow.java
@@ -5,7 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import seedu.addressbook.commands.ClearCommand;
 import seedu.addressbook.commands.ExitCommand;
+import seedu.addressbook.commands.ListCommand;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.data.person.ReadOnlyPerson;
@@ -23,14 +25,14 @@ public class MainWindow {
     private Logic logic;
     private Stoppable mainApp;
 
-    public MainWindow(){
+    public MainWindow() {
     }
 
-    public void setLogic(Logic logic){
+    public void setLogic(Logic logic) {
         this.logic = logic;
     }
 
-    public void setMainApp(Stoppable mainApp){
+    public void setMainApp(Stoppable mainApp) {
         this.mainApp = mainApp;
     }
 
@@ -46,7 +48,7 @@ public class MainWindow {
         try {
             String userCommandText = commandInput.getText();
             CommandResult result = logic.execute(userCommandText);
-            if(isExitCommand(result)){
+            if (isExitCommand(result)) {
                 exitApp();
                 return;
             }
@@ -58,30 +60,74 @@ public class MainWindow {
         }
     }
 
+    @FXML
+    void handleList() {
+        try {
+            CommandResult result = logic.execute(ListCommand.COMMAND_WORD);
+            displayResult(result);
+            clearCommandInput();
+        } catch (Exception e) {
+            display(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void handleClear() {
+        try {
+            CommandResult result = logic.execute(ClearCommand.COMMAND_WORD);
+            displayResult(result);
+            clearCommandInput();
+        } catch (Exception e) {
+            display(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void handleExit() {
+        try {
+            CommandResult result = logic.execute(ExitCommand.COMMAND_WORD);
+            exitApp();
+            return;
+        } catch (Exception e) {
+            display(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
     private void exitApp() throws Exception {
         mainApp.stop();
     }
 
-    /** Returns true of the result given is the result of an exit command */
+    /**
+     * Returns true of the result given is the result of an exit command
+     */
     private boolean isExitCommand(CommandResult result) {
         return result.feedbackToUser.equals(ExitCommand.MESSAGE_EXIT_ACKNOWEDGEMENT);
     }
 
-    /** Clears the command input box */
+    /**
+     * Clears the command input box
+     */
     private void clearCommandInput() {
         commandInput.setText("");
     }
 
-    /** Clears the output display area */
-    public void clearOutputConsole(){
+    /**
+     * Clears the output display area
+     */
+    public void clearOutputConsole() {
         outputConsole.clear();
     }
 
-    /** Displays the result of a command execution to the user. */
+    /**
+     * Displays the result of a command execution to the user.
+     */
     public void displayResult(CommandResult result) {
         clearOutputConsole();
         final Optional<List<? extends ReadOnlyPerson>> resultPersons = result.getRelevantPersons();
-        if(resultPersons.isPresent()) {
+        if (resultPersons.isPresent()) {
             display(resultPersons.get());
         }
         display(result.feedbackToUser);
