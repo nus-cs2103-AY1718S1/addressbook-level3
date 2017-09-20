a163235ecb7635fc6f02c339d7057c3a93ca75b3
Dong Shaocong <shaocong.dong@u.nus.edu>
2017-09-20 09:05:17 +0800

    Add `Add` and `Edit` Command with Material Design

--- src/seedu/addressbook/MainApp.java
+++ src/seedu/addressbook/MainApp.java
@@ -8,12 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.Modality;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 
 import seedu.addressbook.model.Person;
 import seedu.addressbook.view.PersonOverviewController;
-
+import seedu.addressbook.view.PersonEditDialogController;
 
 public class MainApp extends Application {
     private Stage primaryStage;
@@ -95,6 +96,44 @@ public class MainApp extends Application {
             controller.setMainApp(this);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
