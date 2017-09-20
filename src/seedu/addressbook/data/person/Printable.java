8ad2d492d9ea2e6ffa6bb34e2a4272becd882c2b
lys <honestkiddy@gmail.com>
2017-09-20 20:46:59 +0800

    Printable.java: Add printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,7 @@
+package seedu.addressbook.data.person;
+
+public interface Printable {
+
+    //Produces a printable string representation of the object. e.g. Name: John Smith, Phone: 12349862
+    String getPrintableString () ;
+}
