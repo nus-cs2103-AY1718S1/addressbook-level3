e633a92b55e4f83ab76e89114af2a13df7b14b14
Vanessa Lim <31247542+vlimshimin@users.noreply.github.com>
2017-09-21 00:57:35 +0800

    Implemented isMutating() in Commands

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -15,6 +15,7 @@ public abstract class Command {
     protected AddressBook addressBook;
     protected List<? extends ReadOnlyPerson> relevantPersons;
     private int targetIndex = -1;
+    private boolean isMutating = false;
 
     /**
      * @param targetIndex last visible listing index of the target person
@@ -61,6 +62,10 @@ public abstract class Command {
      */
     protected ReadOnlyPerson getTargetPerson() throws IndexOutOfBoundsException {
         return relevantPersons.get(getTargetIndex() - DISPLAYED_INDEX_OFFSET);
+    }
+
+    public boolean isMutating() {
+        return isMutating;
     }
 
     public int getTargetIndex() {
