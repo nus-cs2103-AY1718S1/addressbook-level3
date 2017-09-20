c32eab29e0881e633219c2632633922329e29503
JarylTan\JarylTanZH <exdarkrb@gmail.com>
2017-09-20 10:33:37 +0800

    Implemented the Printable interface and implemented and overrided it in all the relevant classes. Changed the default method in ReadOnlyPerson to use the getPrintableString method implemented in the Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+public interface Printable {
+    /**
+     * @return Returns a concatenated version of the printable strings of each object.
+     */
+    String getPrintableString();
+}
