a604f585e5cba27b47107a6428122ca4bc0e421d
Hu Yuanrong <e0014513@u.nus.edu>
2017-09-21 22:06:57 +0800

    LogicTest.java: Added the JUnit test to add file only if isMutating() is true.

--- test/java/seedu/addressbook/logic/LogicTest.java
+++ test/java/seedu/addressbook/logic/LogicTest.java
@@ -12,6 +12,7 @@ import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.*;
 import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.parser.Parser;
 import seedu.addressbook.storage.StorageFile;
 
 import java.util.*;
@@ -31,6 +32,7 @@ public class LogicTest {
     private StorageFile saveFile;
     private AddressBook addressBook;
     private Logic logic;
+    private Command testCommand;
 
     @Before
     public void setup() throws Exception {
@@ -90,7 +92,11 @@ public class LogicTest {
         //Confirm the state of data is as expected
         assertEquals(expectedAddressBook, addressBook);
         assertEquals(lastShownList, logic.getLastShownList());
-        assertEquals(addressBook, saveFile.load());
+
+        testCommand = new Parser().parseCommand(inputCommand);
+        if (testCommand.isMutating()) {
+            assertEquals(addressBook, saveFile.load());
+        }
     }
 
 
