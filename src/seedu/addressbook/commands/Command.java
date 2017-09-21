7cf8e59e07647fe17c70f80187bd57514edad8a9
Mushroom Wen <Wen Xin@DESKTOP-IVHHP1V.(none)>
2017-09-21 20:58:21 +0800

    W6.1d polymorphism implementation

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -47,6 +47,11 @@ public abstract class Command {
     //public abstract CommandResult execute();
 
     /**
+     * Abstract methods for commands: if the command mutates the data return true
+     */
+    public abstract boolean isMutating();
+    
+    /**
      * Supplies the data the command will operate on.
      */
     public void setData(AddressBook addressBook, List<? extends ReadOnlyPerson> relevantPersons) {
