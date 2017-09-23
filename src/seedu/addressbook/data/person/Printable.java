de29b74891ceb5a32ef0fcf3530200527a74ed86
Jiachao Lin <lin_jiachao@hotmail.com>
2017-09-23 20:25:20 +0800

    Add javadocs for Printable interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.data.person;
 
+/**
+ * Represents a printable entity for all data in the addressBook.
+ * Implementations should guarantee: public String getPrintableString() methods must exist and must return a String.
+ */
 public interface Printable {
 
     String getPrintableString();
