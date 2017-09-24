950807d878df2a207ea96f9423081f2b1890d7e5
APPLETEA\Huijia <hjc2304@gmail.com>
2017-09-24 16:12:22 +0800

    Revert "added printable interface"
    This reverts commit 3ca39eb82657e65e333bfb1e97e8f2591c4d066b.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address implements Printable {
+public class Address {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
@@ -54,9 +54,5 @@ public class Address implements Printable {
 
     public boolean isPrivate() {
         return isPrivate;
-    }
-
-    public String getPrintableString(Printable p, Printable p1, Printable p2) {
-        return value;
     }
 }
--- src/seedu/addressbook/data/person/Printable.java
+++
@@ -1,5 +0,0 @@
-package seedu.addressbook.data.person;
-
-public interface Printable {
-    String getPrintableString(Printable p, Printable p1, Printable p2);
-}
