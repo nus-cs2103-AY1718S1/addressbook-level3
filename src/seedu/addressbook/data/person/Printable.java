175499f33364eb7bdf79b59bdde64de47a75a18b
minato <dmc3hpcity@gmail.com>
2017-09-26 18:28:48 +0800

    Implement Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface for printable string representation of person's data in the addressbook.
+ */
+public interface Printable {
+
+    String getPrintableString();
+
+}
