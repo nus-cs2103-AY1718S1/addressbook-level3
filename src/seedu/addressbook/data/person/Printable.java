bebe1281e1769b799c08a80a9c8856ea82de6295
jaivigneshvenugopal <smugs1995@gmail.com>
2017-09-27 21:08:35 +0800

    Added comments to explain the need for Printable Interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.data.person;
 
+/**
+ * A read-only immutable interface only allows object to be printed.
+ * Implementations should guarantee: only the necessary details for the object are returned for the printing.
+ */
 public interface Printable {
     public String getPrintableString();
 }
