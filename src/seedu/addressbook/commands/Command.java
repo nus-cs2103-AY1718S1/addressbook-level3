d8b58d6fe9b69119bbfe628b3c22d85c68300d5f
jaivigneshvenugopal <smugs1995@gmail.com>
2017-09-27 16:26:03 +0800

    Added a dummy method that throws an exception when called.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -43,6 +43,10 @@ public abstract class Command {
         throw new UnsupportedOperationException("This method should be implement in child classes");
     }
 
+    public boolean isMutating() {
+        throw new UnsupportedOperationException("This method should be implemented in child classes");
+    }
+
     //Note: it is better to make the execute() method abstract, by replacing the above method with the line below:
     //public abstract CommandResult execute();
 
