1cb9b8bad1ac4ef89425e1c0ef3a932a01dbb681
nbriannl <neilbrian.nl@gmail.com>
2017-10-01 14:21:50 +0800

    "add getPrintableString() in ReadOnlyPerson and comment on Printable interface"

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,11 @@
 package seedu.addressbook.data.person;
 
+/**
+ * An interface to provide data into printable format for Person's details in the addressbook.
+ * Implementation should return the details of a person with a prefixed title of the information
+ * Good: return "DataTitle: " + value;
+ * Bad:  return value;
+ */
 public interface Printable {
     String getPrintableString();
 }
--- src/seedu/addressbook/data/person/ReadOnlyPerson.java
+++ src/seedu/addressbook/data/person/ReadOnlyPerson.java
@@ -68,17 +68,28 @@ public interface ReadOnlyPerson {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName());
         if (!getPhone().isPrivate()) {
-            builder.append(" Phone: ").append(getPhone());
+            builder.append(" ").append(getPrintableString(getPhone()));
         }
         if (!getEmail().isPrivate()) {
-            builder.append(" Email: ").append(getEmail());
+            builder.append(" ").append(getPrintableString(getEmail()));
         }
         if (!getAddress().isPrivate()) {
-            builder.append(" Address: ").append(getAddress());
+            builder.append(" ").append(getPrintableString(getAddress()));
         }
         builder.append(" Tags: ");
         for (Tag tag : getTags()) {
             builder.append(tag);
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Returns a concatenated version of the printable strings of each object.
+     */
+    default String getPrintableString(Printable... printables){
+        final StringBuilder builder = new StringBuilder();
+        for (Printable p: printables) {
+            builder.append(p.getPrintableString());
         }
         return builder.toString();
     }
