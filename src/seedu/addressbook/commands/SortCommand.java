ce7300b1cd00e3ed6e5c22dd640808a6270ab570
liuhang0213 <liuhang0213@gmail.com>
2017-10-05 18:41:19 +0800

    Adds optional argument to indicate sorting order

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,8 +1,10 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -15,8 +17,29 @@ public class SortCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
             + "Displays all persons in the address book, sorted by names in alphabetical order.\n\t"
             + "Example: " + COMMAND_WORD;
+    
+    public static final String MESSAGE_INVALID_ARGUMENT = "Sort order should be 'asc' or 'desc'";
 
+    private enum SortOrder {
+        ASC, DESC
+    }
+    
+    // Sort in ascending order by default
+    private SortOrder order = SortOrder.ASC;
 
+    public SortCommand() {
+    }
+    
+    public SortCommand(String sortOrderInput) throws IllegalValueException {
+        if (Objects.equals(sortOrderInput, "asc")) {
+            this.order = SortOrder.ASC;
+        } else if (Objects.equals(sortOrderInput, "desc")) {
+            this.order = SortOrder.DESC;
+        } else {
+            throw new IllegalValueException(MESSAGE_INVALID_ARGUMENT);
+        }
+    }
+    
     @Override
     public CommandResult execute() {
         addressBook.sortAllPersons();
